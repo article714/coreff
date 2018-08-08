197e5f6b066f539ae40ce084d3e64367f77d9e0c
Julien <julien@article714.org>
2018-08-08 16:10:49 +0200

    [ADD]Commit main structure with CorefPartner class

---
+++ models/coref_partner.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 8 August 2018
+
+@author: J. Carette
+@copyright: ©2018 Article 714
+@license: AGPL v3
+'''
+
+from . import models, fields, api
+
+class coreffPartner():
+    _name='coref_partner'
+    _inherits={'res.partner':'partner_id'}
+    
+    #Add all fields from CreditSafe request
+    
+    
+    #Add all fields from Societe.com request
+
