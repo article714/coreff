a4118d9415faac4d9eb9703c7695abb595073b12
xtof <xtof@hector3>
2018-09-06 20:37:14 +0200

    [FIX] prevent crash when initializing module on non internet-connected host

--- coreff_creditsafe/models/creditsafe_data_wsdl.py
+++ coreff_creditsafe/models/creditsafe_data_wsdl.py
@@ -21,10 +21,10 @@ from odoo import exceptions
 # global vars
 wsdl = 'https://www.creditsafe.fr/getdata/service/CSFRServices.asmx?wsdl'
 
-#<header> 
-#<username>Login</username> 
-#<password>Password</password> 
-#<operation>Operation</operation> 
+#<header>
+#<username>Login</username>
+#<password>Password</password>
+#<operation>Operation</operation>
 #<language>Langue</language>
 #<country>Pays</country>
 #<chargereference>Reference utilisateur</chargereference>
@@ -36,7 +36,7 @@ header_values = {
     'operation': 'getcompanyinformation',
     'language': 'FR',
     'country': 'FR',
-    'chargereference': ''    
+    'chargereference': ''
 }
 
 body_values = {
@@ -53,64 +53,66 @@ class ZeepClient(object):
         self.header_elements = []
 
         self.lock = threading.Lock()
-        #complete header values
+        # complete header values
         header = Element('header')
         for k in header_values:
             a = Element(k)
             a.text = header_values[k]
-            #self.header_elements.append(a)
+            # self.header_elements.append(a)
             header.append(a)
 
-        #complete body values
+        # complete body values
         body = Element('body')
         for l in body_values:
             b = Element(l)
             b.text = body_values[l]
             body.append(b)
-        
+
         self.xml_request.append(header)
         self. xml_request.append(body)
-        self.client = zeep.Client(wsdl=wsdl)
+        try:
+            self.client = zeep.Client(wsdl=wsdl)
+        except:
+            logging.error("NO Zeep Client initialized")
+            self.client = None
 
-        #set namespace to Zeep Client
+        # set namespace to Zeep Client
         #self.client.set_ns_prefix('NS1', 'http://NAMESPACE_URL')
 
-    def getCompanyInformation(self, siret, ref = '', operation = 'getcompanyinformation'):
+    def getCompanyInformation(self, siret, ref='', operation='getcompanyinformation'):
         self.lock.acquire()
         try:
-            #complete header with specific values
-            
+            # complete header with specific values
+
             o = self.xml_request.xpath('/xmlrequest/header/operation')
             o[0].text = operation
-            
+
             if(ref != ''):
                 c = self.xml_request.xpath('/xmlrequest/header/chargereference')
                 c[0].text = ref
-             
+
             if(siret != None and siret != ''):
                 s = self.xml_request.xpath('/xmlrequest/body/companynumber')
                 s[0].text = siret
-            
+
             response = self.client.service.GetData(tostring(self.xml_request, pretty_print=True))
-            
-            if response != None: 
-                
-                #Besoin d'encoder en UTF-8 avant lecture 
+
+            if response != None:
+
+                # Besoin d'encoder en UTF-8 avant lecture
                 xml = response.encode("utf-8")
                 xml_response = fromstring(response)
-                    
-                report_type  = xml_response.xpath('/xmlresponse/header/reportinformation/reporttype') 
+
+                report_type = xml_response.xpath('/xmlresponse/header/reportinformation/reporttype')
                 if report_type != None and report_type[0].text == str(operation):
                     return xml_response
                 else:
                     error_detail = xml_response.xpath('/xmlresponse/body/errors/errordetail')
                     code = error_detail[0].find('code').text
                     desc = error_detail[0].find('desc').text
-                    raise Exception("Erreur lors de l'execution du service Credit Safe - CODE : %s - LABEL : %s" % (code, desc))
-            
-            
+                    raise Exception(
+                        "Erreur lors de l'execution du service Credit Safe - CODE : %s - LABEL : %s" % (code, desc))
 
-        
         except zeep.exceptions.Fault as e:
             logging.warning('Fault when PROCESSING Credit-Safe Service --> %s' % str(e))
             return None
@@ -122,14 +124,13 @@ class ZeepClient(object):
             self.lock.release()
 
 
-
 zeep_client = ZeepClient()
 
 
 def get_company_information_by_siret(siret, ref):
-    
+
     if zeep_client != None:
-   
+
         return zeep_client.getCompanyInformation(siret, ref)
     else:
         logging.warning('Fault when PROCESSING Credit-Safe Service --> No Zeep Client found')
