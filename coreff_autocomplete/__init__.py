94285cc4ed0cd6e2dc3ac9dc6e1fbe67510e20fe
Loïc KERARVRAN <lkerarvran@pennarbed.net>
2019-07-23 10:06:04 +0200

    [LKE] : coreff autocomplete

---
+++ coreff_autocomplete/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @author: L. Kerarvran
+# @copyright: ©2018-2019 Article 714
+# @license: AGPL v3
+
+from . import models
---
+++ coreff_autocomplete/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@author: L. Kerarvran
+@copyright: ©2018-2019 Article 714
+@license: AGPL v3
+"""
+
+from . import creditsafe_authent
+from . import res_config_settings
