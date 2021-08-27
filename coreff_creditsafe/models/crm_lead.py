2df290a2a0f89e1817e1757723180f30f0991c04
Chris Mann <chrisandrewmann@gmail.com>
2021-08-27 16:30:57 +0100

    [FIX] Prevent lead creditsafe lookup button executing and crashing if company name is missing

--- coreff_creditsafe/models/crm_lead.py
+++ coreff_creditsafe/models/crm_lead.py
@@ -14,13 +14,17 @@ class CrmLead(models.Model):
     _inherit = "crm.lead"
 
     def creditsafe_lookup(self):
-        company_name = urllib.parse.quote(self.partner_name)
-        creditsafe_url = "https://app.creditsafe.com/search"
-        creditsafe_params = f"?limit=15&name={company_name}&page=1"
+        if self.partner_name != False:
+            company_name = urllib.parse.quote(self.partner_name)
+            creditsafe_url = "https://app.creditsafe.com/search"
+            creditsafe_params = f"?limit=15&name={company_name}&page=1"
 
-        action = {
-            "type"     : "ir.actions.act_url",
-            "target"   : "new",
-            "url"      : creditsafe_url + creditsafe_params
-        }
-        return action
+            action = {
+                "type"     : "ir.actions.act_url",
+                "target"   : "new",
+                "url"      : creditsafe_url + creditsafe_params
+            }
+            return action
+        else:
+            raise UserError("Missing [Company Name] to perform Creditsafe lookup.")
+            return False
